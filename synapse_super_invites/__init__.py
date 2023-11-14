4c5b2ab9314cf7cb09ad2da00a3e38fa97586de4
Benjamin Kampmann <ben@gnunicorn.org>
2023-11-14 18:53:06 +0000

    Add database layout maker and miration

--- synapse_super_invites/__init__.py
+++ synapse_super_invites/__init__.py
@@ -1,9 +1,26 @@
 from typing import Any, Dict
 
 import attr
+import os
 from synapse.config import ConfigError
 from synapse.module_api import ModuleApi
 
+import alembic.config
+from .model import Token
+
+def do_upgrade(revision, context):
+    return alembic_script._upgrade_revs(script.get_heads(), revision)
+
+def run_alembic(sql_url):
+    from alembic.config import Config
+    from alembic import command, autogenerate
+    from alembic.script import ScriptDirectory
+    from alembic.runtime.environment import EnvironmentContext
+
+    alembic_cfg = Config()
+    alembic_cfg.set_main_option("script_location", os.path.join(os.path.dirname(os.path.realpath(__file__)), "migration"))
+    alembic_cfg.set_main_option("sqlalchemy.url", sql_url)
+    alembic.command.upgrade(alembic_cfg, 'head')
 
 @attr.define
 class SynapseSuperInvitesConfig:
@@ -14,6 +31,7 @@ class SynapseSuperInvitesConfig:
 class SynapseSuperInvites:
     def __init__(self, config: SynapseSuperInvitesConfig, api: ModuleApi):
         # Keep a reference to the config and Module API
+        run_alembic(config.sql_url)
         self._api = api
         self._config = config
 
---
+++ synapse_super_invites/model/__init__.py
@@ -0,0 +1,76 @@
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey, String, Table, Column, Boolean, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from uuid import uuid4
+
+def uuid_short() -> str:
+    return str(uuid.uuid4()).split("-")[0]
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+token_rooms = Table(
+    "token_room_associations",
+    Base.metadata,
+    Column("token", ForeignKey("tokens.token"), primary_key = True),
+    Column("room", ForeignKey("rooms.nameOrAlias"), primary_key = True),
+)
+
+class Token(Base):
+    __tablename__ = "tokens"
+    token: Mapped[str] = mapped_column(
+        String(50), default=uuid_short, primary_key=True
+    )
+    owner: Mapped[str] = mapped_column(String(255))
+    create_dm: Mapped[bool] = mapped_column(Boolean)
+
+    rooms: Mapped[List['Room']] = relationship(
+        secondary=token_rooms, back_populates="tokens"
+    )
+
+    # meta
+    created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at = mapped_column(
+        DateTime(timezone=True), 
+        server_default=func.now(), 
+        onupdate=func.now())
+    deleted_at = mapped_column(DateTime(timezone=True), default = None)
+    
+    def __repr__(self) -> str:
+        return f"Token({self.token!r}, owned={self.user!r})"
+
+class Room(Base):
+    __tablename__ = "rooms"
+    nameOrAlias: Mapped[str] = mapped_column(
+        String(255), primary_key=True
+    )
+
+    tokens: Mapped[List[Token]] = relationship(
+        secondary=token_rooms, back_populates="rooms"
+    )
+
+    # meta
+    created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at = mapped_column(
+        DateTime(timezone=True), 
+        server_default=func.now(), 
+        onupdate=func.now())
+
+
+class Accepted(Base):
+    __tablename__ = "accepted"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user: Mapped[str] = mapped_column(String(255))
+
+    token_id = mapped_column(ForeignKey("tokens.token"))
+    token = relationship("Token", back_populates="accepted")
+
+    # meta
+    created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at = mapped_column(
+        DateTime(timezone=True), 
+        server_default=func.now(), 
+        onupdate=func.now())
